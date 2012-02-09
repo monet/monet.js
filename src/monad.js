33c1996136b9c7af1a69762d906d9c4af5430c1c
Myers <myersch@OSTMLADM0125737.local>
2012-02-09 17:27:00 +0000

    Added isSome and isNone functionsto Option

--- src/monad.js
+++ src/monad.js
@@ -6,14 +6,30 @@ function Some(val) {
     if (val == null) {
         throw "Illegal state expection"
     }
-     this.val = val
+    this.val = val
 }
 
-None.prototype.map = function() {
+None.prototype.map = function () {
     return this
 }
 
-Some.prototype.map = function(fn) {
+Some.prototype.map = function (fn) {
     return new Some(fn(this.val))
+}
+
+None.prototype.isSome = function () {
+    return false
+}
+
+Some.prototype.isSome = function () {
+    return true
+}
+
+None.prototype.isNone = function () {
+    return true
+}
+
+Some.prototype.isNone = function () {
+    return false
 }
 
