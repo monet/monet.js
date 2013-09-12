de589f0ade6935d3c27076acce689f14c5b270ba
Chris Myers <christopher.myers@bskyb.com>
2013-09-12 20:37:45 +0100

    Added monad transformer tests for applicatives

--- src/test/javascript/monad_transformer_spec.js
+++ src/test/javascript/monad_transformer_spec.js
@@ -1,5 +1,7 @@
 describe('A Monad Transformer', function () {
 
+    Functional.install()
+
     beforeEach(function() {
         this.addMatchers({
             toBeSomeMaybe: function(expected) {
@@ -14,10 +16,29 @@ describe('A Monad Transformer', function () {
         });
     });
 
+    function create(a) {
+        return monadT(IO(function() {return Maybe.some(a)}))
+    }
+    var maybeIO = create("hi")
+
     it('will allow a containing value to be mapped', function() {
-        var maybeIO = monadT(IO(function() {return Maybe.some("hi")}))
         var mappedMaybeIO = maybeIO.map(function (s) { return s+" world"})
 
         expect(mappedMaybeIO.perform().run()).toBeSomeMaybeWith("hi world")
+    })
+
+    it('will allow a containing value to be flatMapped', function() {
+        var flatmappedMaybeIO = maybeIO.flatMap(function (s) { return Maybe.some("w00t")})
+        expect(flatmappedMaybeIO.perform().run()).toBeSomeMaybeWith("w00t")
+    })
+
+    it('will allow a containing value to be applied (applicative functor style)', function() {
+        var plus = function(a,b) { return a + b}.partial(_,_)
+        var maybeIOWithFn = create(plus)
+        var maybeIO1 = create(20)
+        var maybeIO2 = create(22)
+        var maybeIOResult = maybeIO2.ap(maybeIO1.ap(maybeIOWithFn))
+        expect(maybeIOResult.perform().run()).toBeSomeMaybeWith(42)
+
     })
 })
