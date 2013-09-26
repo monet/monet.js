fbdfc4e81b22cfdec91d126206a9cecf184180df
Chris Myers <chris@chrisandjo.com>
2013-09-26 22:20:43 +0100

    More Tests

--- src/test/javascript/list_spec.js
+++ src/test/javascript/list_spec.js
@@ -1,5 +1,14 @@
 describe("An immutable list", function () {
     var list = List(1, List(2, List(3, List(4, Nil))))
+
+    it("can be converted to Array", function() {
+        expect(list.toArray()).toEqual([1,2,3,4])
+    })
+
+    it("can be created from an Array", function(){
+        expect([1,2,3,4].list()).toEqual(list)
+    })
+
     it("can be mapped", function () {
         var mappedList = list.map(function (a) {
             return a + 1
@@ -8,5 +17,9 @@ describe("An immutable list", function () {
         expect(mappedList.tail.head).toBe(3)
         expect(mappedList.tail.tail.head).toBe(4)
         expect(mappedList.tail.tail.tail.head).toBe(5)
+    })
+
+    it("can be reduced using foldLeft", function() {
+
     })
 })
