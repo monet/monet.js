15dd5d0432cc4e57d812b5cb70fe51395cadc38d
Jakub Strojewski <jakub.strojewski@gmail.com>
2017-01-03 08:39:39 +0100

    53: Clenup code style in tests

--- test/monad-laws-spec.js
+++ test/monad-laws-spec.js
@@ -1,79 +1,89 @@
 describe('The Monad', function () {
 
-    var test = function(monad) {
-      var equals = function(a, b) {
-        if (a.equals) {
-          return a.equals(b);
+    var test = function (monad) {
+        var equals = function (a, b) {
+            if (a.equals) {
+                return a.equals(b);
+            }
+            return a.run() === b.run()
         }
-        return a.run() === b.run()
-      }
 
-      var fa = function (x) {
-        return monad.of(x + 1)
-      }
+        var fa = function (x) {
+            return monad.of(x + 1)
+        }
 
-      it("must obey left identity", function() {
+        it('must obey left identity', function () {
 
-        var f = monad.of(123).bind(fa)
-        var fapply = fa(123)
-        expect(equals(f, fapply)).toBeTruthy()
-      })
+            var f = monad.of(123).bind(fa)
+            var fapply = fa(123)
+            expect(equals(f, fapply)).toBeTruthy()
+        })
 
-      it("must obey right identity", function() {
-        var m = monad.of(123)
-        var m1 = m.bind(function (x) { return monad.of(x)})
+        it('must obey right identity', function () {
+            var m = monad.of(123)
+            var m1 = m.bind(function (x) {
+                return monad.of(x)
+            })
 
-        expect(equals(m, m1)).toBeTruthy()
+            expect(equals(m, m1)).toBeTruthy()
 
-      })
+        })
 
-      it("must be associative", function() {
-        var m = monad.of(123)
-        var a = (m.bind(function(x) { return monad.of(x*2)})).bind(fa)
-        var b = (m.bind(function(x) { return monad.of(x*2).bind(fa)}))
-        expect(equals(a, b)).toBeTruthy()
+        it('must be associative', function () {
+            var m = monad.of(123)
+            var a = (m.bind(function (x) {
+                return monad.of(x * 2)
+            })).bind(fa)
+            var b = (m.bind(function (x) {
+                return monad.of(x * 2).bind(fa)
+            }))
+            expect(equals(a, b)).toBeTruthy()
 
-      })
+        })
 
-      it("must also be applicative", function() {
-        var m = monad.of(123)
-        var f = function(t) { return 2*t }
-        var mf = monad.of(f)
-        var a = m.ap(mf)
-        var b = m.bind(function(t) { return m.unit(f(t)) })
-        expect(equals(a, b)).toBeTruthy()
-      })
+        it('must also be applicative', function () {
+            var m = monad.of(123)
+            var f = function (t) {
+                return 2 * t
+            }
+            var mf = monad.of(f)
+            var a = m.ap(mf)
+            var b = m.bind(function (t) {
+                return m.unit(f(t))
+            })
+            expect(equals(a, b)).toBeTruthy()
+        })
     }
 
-    describe('Maybe', function() {
-      test(Maybe)
+    describe('Maybe', function () {
+        test(Maybe)
     })
 
-    describe('Either', function() {
-      test(Either)
+    describe('Either', function () {
+        test(Either)
     })
 
-    describe('IO', function() {
-      test(IO)
+    describe('IO', function () {
+        test(IO)
     })
-    describe('Reader', function() {
-     test(Reader)
-    })
-
-    describe('List', function() {
-      test(List)
+    describe('Reader', function () {
+        test(Reader)
     })
 
-    describe('NonEmptyList', function() {
-      test(NEL)
+    describe('List', function () {
+        test(List)
     })
 
-    describe('Free', function() {
-      test(Free)
+    describe('NonEmptyList', function () {
+        test(NEL)
     })
 
-    describe('Validation', function() {
-      test(Validation)
+    describe('Free', function () {
+        test(Free)
+    })
+
+    describe('Validation', function () {
+        test(Validation)
     })
 
 })
--- test/reader-spec.js
+++ test/reader-spec.js
@@ -1,71 +1,73 @@
-describe("A Reader Monad", function () {
+describe('A Reader Monad', function () {
 
-  var config1 = {
-    url: "http://test1.com",
-    port: 8080,
-    key: "s3cr3t"
-  }
-  var config2 = {
-    url: "http://test2.com",
-    port: 8081,
-    key: "not so secret"
-  }
-  var connect = function (endpoint) {
-    return Reader(function (config) {
-      return "POST " + config.url + ":" + config.port + "/" + endpoint
-    })
-  }
+    var config1 = {
+        url: 'http://test1.com',
+        port: 8080,
+        key: 's3cr3t'
+    }
+    var config2 = {
+        url: 'http://test2.com',
+        port: 8081,
+        key: 'not so secret'
+    }
+    var connect = function (endpoint) {
+        return Reader(function (config) {
+            return 'POST ' + config.url + ':' + config.port + '/' + endpoint
+        })
+    }
 
-  it("must inject config with run()", function () {
-    expect(connect("user").run(config1)).toBe("POST http://test1.com:8080/user")
-    expect(connect("session").run(config2)).toBe("POST http://test2.com:8081/session")
-  })
-
-  it("must be mappable", function () {
-    var reader = connect("session").map(function (s) {
-      return s + "?secure=true"
-    })
-    expect(reader.run(config1)).toBe("POST http://test1.com:8080/session?secure=true")
-
-  })
-
-  it("must be flatMappable", function () {
-    var reader = connect("something").flatMap(function (connectionString) {
-      return Reader(function (config) {
-        return connectionString + "?secretKey=" + config.key
-      })
-    })
-    expect(reader.run(config1)).toBe("POST http://test1.com:8080/something?secretKey=s3cr3t")
-  })
-
-  it("must be applicable", function () {
-    var reader = connect("something").ap(Reader(function (config) {
-      return function (c) {
-        return "**" + c + "**"
-      }
-
-    }))
-    expect(reader.run(config1)).toBe("**POST http://test1.com:8080/something**")
-
-  })
-
-  it("must support local", function () {
-    var reader = connect("something").flatMap(function (connectionString) {
-      return Reader(function (key) {
-        return connectionString + "?secretKey=" + key
-      }).local(function(c) {return c.key})
+    it('must inject config with run()', function () {
+        expect(connect('user').run(config1)).toBe('POST http://test1.com:8080/user')
+        expect(connect('session').run(config2)).toBe('POST http://test2.com:8081/session')
     })
 
-    expect(reader.run(config1)).toBe("POST http://test1.com:8080/something?secretKey=s3cr3t")
+    it('must be mappable', function () {
+        var reader = connect('session').map(function (s) {
+            return s + '?secure=true'
+        })
+        expect(reader.run(config1)).toBe('POST http://test1.com:8080/session?secure=true')
 
-  })
-
-  it("can ask for config", function() {
-    var reader = Reader.ask().map(function(config) {
-      return config.key
     })
-    expect(reader.run(config1)).toBe("s3cr3t")
 
-  })
+    it('must be flatMappable', function () {
+        var reader = connect('something').flatMap(function (connectionString) {
+            return Reader(function (config) {
+                return connectionString + '?secretKey=' + config.key
+            })
+        })
+        expect(reader.run(config1)).toBe('POST http://test1.com:8080/something?secretKey=s3cr3t')
+    })
+
+    it('must be applicable', function () {
+        var reader = connect('something').ap(Reader(function (config) {
+            return function (c) {
+                return '**' + c + '**'
+            }
+
+        }))
+        expect(reader.run(config1)).toBe('**POST http://test1.com:8080/something**')
+
+    })
+
+    it('must support local', function () {
+        var reader = connect('something').flatMap(function (connectionString) {
+            return Reader(function (key) {
+                return connectionString + '?secretKey=' + key
+            }).local(function (c) {
+                return c.key
+            })
+        })
+
+        expect(reader.run(config1)).toBe('POST http://test1.com:8080/something?secretKey=s3cr3t')
+
+    })
+
+    it('can ask for config', function () {
+        var reader = Reader.ask().map(function (config) {
+            return config.key
+        })
+        expect(reader.run(config1)).toBe('s3cr3t')
+
+    })
 
 })
