575dafec572b7e01bc9071ee969a7f9771207964
Jakub Strojewski <jakub.strojewski@gmail.com>
2017-06-07 07:58:26 +0200

    Update dependencies

--- dist/monet.js
+++ dist/monet.js
@@ -9,13 +9,11 @@
 (function(root, factory) {
     if (typeof define === "function" && define.amd) {
         define(factory);
+    } else if (typeof module === "object" && module.exports) {
+        module.exports = factory();
     } else {
-        if (typeof module === "object" && module.exports) {
-            module.exports = factory();
-        } else {
-            root.notUseMonetGlobalObject = !root.useMonetGlobalObject;
-            root.Monet = factory(root);
-        }
+        root.notUseMonetGlobalObject = !root.useMonetGlobalObject;
+        root.Monet = factory(root);
     }
 })(this, function(rootGlobalObject) {
     "use strict";
