21e666daabe5b830624ef15b3d7819597012e167
Jakub Strojewski <jakub.strojewski@gmail.com>
2017-06-19 20:36:51 +0200

    Publish on merge to master

---
+++ scripts/publish.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+VERSION=$(cat package.json \
+ | grep version \
+ | head -1 \
+ | awk -F: '{ print $2 }' \
+ | sed 's/[",]//g' \
+ | tr -d '[[:space:]]')
+
+if [[ $VERSION == *"alpha"* ]]
+then
+  TAG=alpha
+elif [[ $VERSION == *"beta"* ]]
+then
+  TAG=next
+else
+  TAG=latest
+fi
+
+npm publish --tag "$TAG"
